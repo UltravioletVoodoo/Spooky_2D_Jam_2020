4468c77f169d7325461b4b7543acf507377180f3
Torrey Randolph <trandolph123@gmail.com>
2020-10-25 04:21:12 -0700

    add period at the end

--- src/scenes/MainMenuScene.ts
+++ src/scenes/MainMenuScene.ts
@@ -66,7 +66,7 @@ export default class MainMenuScene extends Phaser.Scene {
             this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
                 this.scene.start(QuoteScene.name, {
                     delay: 7000,
-                    quote: `“After a loved one dies, there comes a time when you must move on.\n              Not long after, they must learn to move on too”`,
+                    quote: `“After a loved one dies, there comes a time when you must move on.\n              Not long after, they must learn to move on too.”`,
                     callback: (scene: Phaser.Scene) => {
                         scene.scene.start(LevelScene.name);
                     },
